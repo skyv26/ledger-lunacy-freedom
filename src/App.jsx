6a64a9d9ee379f23e0f503b5ecfdd408f18850ca
Aakash Verma <dev.aakashv@gmail.com>
2023-12-22 00:15:07 +0530

    `feat`: add the `Header` and `Balance` component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import Header from './components/Header/Header';
 import TransactionTable from './components/TransactionTable/TransactionTable';
+import Balance from './components/Balance/Balance';
 
-const App = () => <TransactionTable />;
+const App = () => (
+  <>
+    <Header />
+    <Balance />
+    <TransactionTable />
+  </>
+);
 
 export default App;
